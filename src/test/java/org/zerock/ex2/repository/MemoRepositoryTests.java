adfbe0a1c3f6c333a8c460941ccd9c384ace7190
Heonjun Jeong <gjswns1991@gmail.com>
2022-02-02 17:46:27 +0900

    페이징 처리 / 정렬 테스트

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
@@ -70,5 +74,72 @@ class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault(){
+
+        //1페이지의 데이터 10개
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        System.out.println("-----------------------------");
+
+        //총 몇 페이지
+        System.out.println("Total Pages: "+result.getTotalPages());
+
+        //전체 개수
+        System.out.println("Total Count: "+result.getTotalElements());
+
+        //현재 페이지 번호 0부터 시작
+        System.out.println("Page Number: " + result.getNumber());
+
+        //페이지당 데이터 개수
+        System.out.println("Page Size: " + result.getSize());
+
+        //다음 페이지 존재 여부
+        System.out.println("has next page?: " + result.hasNext());
+
+        //시작 페이지(0) 여부
+        System.out.println("first page?: "+result.isFirst());
+
+        System.out.println("------------------------------");
+
+        //getContent() : List<엔티티 타입>으로 반환
+        for(Memo memo :result.getContent()){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort(){
+
+        Sort sort1 = Sort.by("mno").descending(); //역순 정렬
+
+        Pageable pageable = PageRequest.of(0,10,sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        //get() : Stream<엔티티 타입>을 반환
+        result.get().forEach(memo->{
+            System.out.println(memo);
+        });
+
+        Sort sort2 = Sort.by("memoText").ascending(); //순차적 정렬
+
+        Sort sortAll = sort1.and(sort2); //and를 이용한 연결
+
+        //결합된 정렬조건 사용
+        Pageable pageable2 = PageRequest.of(0,10,sortAll);
+
+        result = memoRepository.findAll(pageable2);
+
+        //get() : Stream<엔티티 타입>을 반환
+        result.get().forEach(memo->{
+            System.out.println(memo);
+        });
     }
 }
